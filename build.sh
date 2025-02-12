0acb1e127d976f2d9300e701f2de5c8583bd7243
Lcdemi <lcd4910@g.rit.edu>
2025-02-11 19:25:36 -0500

    Builds whole infra except for pfSense Router

--- build.sh
+++ build.sh
@@ -17,9 +17,9 @@ print_message() {
 # 1. Two private networks for the Blue and Red Team
 # 2. 3 Windows Server 2019 VMs (DC, IIS/FTP, CA)
 # 3. 7 Ubuntu VMs (SQL, Samba, OSSEC, NTP, Apache, Mail, Worker)
-# 4. 1 pfSense Router
-# 5. 10 Kali VMs (Red Team)
-# 6. 1 Gray Team Container
+# 4. 10 Kali VMs (Red Team)
+# 5. 1 Gray Team Container
+# 6. 1 pfSense Router (CURRENTLY UNFINISHED)
 
 # Sets Gray Team Information
 GRAY_TEAM="foxtrot-grayteam"
@@ -167,8 +167,124 @@ ${DC} \\
 --device \"eth0,ipv4.address=${DC_IP}\" \\
 --device \"root,size=320GiB\""
 
-# Creates Red Team Container
+# Creates Windows CA VM
+print_message "Creating CA VM..."
+print_command "incus launch oszoo:winsrv/2019/ansible-cloud \\
+${CA} \\
+--vm \\
+--config limits.cpu=8 \\
+--config limits.memory=16GiB \\
+--network \"${BLUE_NETWORK}\" \\
+--device \"eth0,ipv4.address=${CA_IP}\" \\
+--device \"root,size=320GiB\""
+
+# Creates Windows IIS VM
+print_message "Creating IIS VM..."
+print_command "incus launch oszoo:winsrv/2019/ansible-cloud \\
+${IIS} \\
+--vm \\
+--config limits.cpu=8 \\
+--config limits.memory=16GiB \\
+--network \"${BLUE_NETWORK}\" \\
+--device \"eth0,ipv4.address=${IIS_IP}\" \\
+--device \"root,size=320GiB\""
+
+# Creates Ubuntu Apache VM
+print_message "Creating Apache VM..."
+print_command "incus launch images:ubuntu/noble ${Apache} \\
+--vm \\
+--network \"${BLUE_NETWORK}\" \\
+--device \"eth0,ipv4.address=${Apache_IP}\" -t c4-m8"
+
+# Creates Ubuntu SQL VM
+print_message "Creating SQL VM..."
+print_command "incus launch images:ubuntu/noble ${SQL} \\
+--vm \\
+--network \"${BLUE_NETWORK}\" \\
+--device \"eth0,ipv4.address=${SQL_IP}\" -t c4-m8"
+
+# Creates Ubuntu Mail VM
+print_message "Creating Mail VM..."
+print_command "incus launch images:ubuntu/noble ${Mail} \\
+--vm \\
+--network \"${BLUE_NETWORK}\" \\
+--device \"eth0,ipv4.address=${Mail_IP}\" -t c4-m8"
+
+# Creates Ubuntu NTP VM
+print_message "Creating NTP VM..."
+print_command "incus launch images:ubuntu/noble ${NTP} \\
+--vm \\
+--network \"${BLUE_NETWORK}\" \\
+--device \"eth0,ipv4.address=${NTP_IP}\" -t c4-m8"
+
+# Creates Ubuntu Samba VM
+print_message "Creating Samba VM..."
+print_command "incus launch images:ubuntu/noble ${Samba} \\
+--vm \\
+--network \"${BLUE_NETWORK}\" \\
+--device \"eth0,ipv4.address=${Samba_IP}\" -t c4-m8"
+
+# Creates Ubuntu OSSEC VM
+print_message "Creating OSSEC VM..."
+print_command "incus launch images:ubuntu/noble ${OSSEC} \\
+--vm \\
+--network \"${BLUE_NETWORK}\" \\
+--device \"eth0,ipv4.address=${OSSEC_IP}\" -t c4-m8"
+
+# Creates Ubuntu OSSEC Worker VM
+print_message "Creating OSSEC Worker VM..."
+print_command "incus launch images:ubuntu/noble ${Worker} \\
+--vm \\
+--network \"${BLUE_NETWORK}\" \\
+--device \"eth0,ipv4.address=${Worker_IP}\" -t c4-m8"
+
+# Creates Red Team Containers
 print_message "Creating Red Team Container (1)..."
 print_command "incus launch images:kali ${Nether1} \\
 --network \"${RED_NETWORK}\" \\
 --device \"eth0,ipv4.address=${Nether1_IP}\" -t c4-m8"
+
+print_message "Creating Red Team Container (2)..."
+print_command "incus launch images:kali ${Nether2} \\
+--network \"${RED_NETWORK}\" \\
+--device \"eth0,ipv4.address=${Nether2_IP}\" -t c4-m8"
+
+print_message "Creating Red Team Container (3)..."
+print_command "incus launch images:kali ${Nether3} \\
+--network \"${RED_NETWORK}\" \\
+--device \"eth0,ipv4.address=${Nether3_IP}\" -t c4-m8"
+
+print_message "Creating Red Team Container (4)..."
+print_command "incus launch images:kali ${Nether4} \\
+--network \"${RED_NETWORK}\" \\
+--device \"eth0,ipv4.address=${Nether4_IP}\" -t c4-m8"
+
+print_message "Creating Red Team Container (5)..."
+print_command "incus launch images:kali ${Nether5} \\
+--network \"${RED_NETWORK}\" \\
+--device \"eth0,ipv4.address=${Nether5_IP}\" -t c4-m8"
+
+print_message "Creating Red Team Container (6)..."
+print_command "incus launch images:kali ${Nether6} \\
+--network \"${RED_NETWORK}\" \\
+--device \"eth0,ipv4.address=${Nether6_IP}\" -t c4-m8"
+
+print_message "Creating Red Team Container (7)..."
+print_command "incus launch images:kali ${Nether7} \\
+--network \"${RED_NETWORK}\" \\
+--device \"eth0,ipv4.address=${Nether7_IP}\" -t c4-m8"
+
+print_message "Creating Red Team Container (8)..."
+print_command "incus launch images:kali ${Nether8} \\
+--network \"${RED_NETWORK}\" \\
+--device \"eth0,ipv4.address=${Nether8_IP}\" -t c4-m8"
+
+print_message "Creating Red Team Container (9)..."
+print_command "incus launch images:kali ${Nether9} \\
+--network \"${RED_NETWORK}\" \\
+--device \"eth0,ipv4.address=${Nether9_IP}\" -t c4-m8"
+
+print_message "Creating Red Team Container (10)..."
+print_command "incus launch images:kali ${Nether10} \\
+--network \"${RED_NETWORK}\" \\
+--device \"eth0,ipv4.address=${Nether10_IP}\" -t c4-m8"
